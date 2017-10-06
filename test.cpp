9aec863cde77de622500f1c0962bfe0d89ccc02c
Linlin Xu <linlin.xu@live.com>
2017-10-05 22:24:39 -0400

    Test performance.

--- test.cpp
+++ test.cpp
@@ -15,11 +15,11 @@ START
 	p->init(DIM, true, true); // DIM dimensions, random start, randomly permuted
 	//printf("Running...\n");
 
-	for (size_t i = 1; i <= N * 100; i++)
+	for (size_t i = 1; i <= N; i++)
 	{
 		//Generate the ith vector in the halton sequence, the (i-1)th vector will be discarded
 		p->genHalton();
-		pmt->genrand64_real3();
+		//pmt->genrand64_real3();
 		//for(uint32 j = 1; j <= DIM; j++)
 		//	//Use the jth dimension of the ith vector
 		//	fprintf(stderr, "%1.16f ", p -> get_rnd(j)); 
